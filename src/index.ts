ac6c765f85bd404752981e57182d062354e78a40
rcupull <59990075+rcupull@users.noreply.github.com>
2024-01-27 09:47:03 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || "4009";
 const ENV = process.env.ENV || "prod";
 const DOC = process.env.DOC;
 
-const app = express();
+const expressApp = express();
 
 if (DOC === "true") {
-  app.use(
+  expressApp.use(
     "/api-docs",
     swaggerUiExpress.serve,
     swaggerUiExpress.setup(require("./features/swagger/swagger_output.json"), {
@@ -19,18 +19,22 @@ if (DOC === "true") {
   );
 }
 
-app.use(
+expressApp.use(
   cors({ origin: "http://local.elocality.com:3000", optionsSuccessStatus: 200 })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use("/", mainRouter);
+expressApp.use(express.json());
+expressApp.use(express.urlencoded({ extended: false }));
+expressApp.use("/", mainRouter);
 
-app.get("/some-data", (request, response) => {
+expressApp.get("/some-data", (request, response) => {
   response.send("Hello world");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
-});
+if (ENV === "dev") {
+  expressApp.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`);
+  });
+}
+
+export const app = expressApp;
